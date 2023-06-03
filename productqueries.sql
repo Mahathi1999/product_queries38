8e13a94c956c21965f95e5a47902f94aa5a78e27
Mahathi1999 <124342355+Mahathi1999@users.noreply.github.com>
2023-06-03 18:26:13 +0530

    Update productqueries.sql

--- productqueries.sql
+++ productqueries.sql
@@ -1,33 +1,30 @@
 /*Find all the information about each products*/
-SELECT * FROM Products;
+db.products.find({})
 
 /*Find the product price which are between 400 to 800*/
-SELECT * FROM Products WHERE product_price BETWEEN 400 AND 800;
+db.products.find({ price: { $gte: 400, $lte: 800 } })
 
 /*Find the product price which are not between 400 to 600*/
-SELECT * FROM Products WHERE product_price NOT BETWEEN 400 AND 600;
+db.products.find({ $or: [{ price: { $lt: 400 } }, { price: { $gt: 600 } }] })
 
 /*List the four product which are grater than 500 in price */
-SELECT * FROM Products WHERE product_price > 500 LIMIT 4;
+db.products.find({ price: { $gt: 500 } }).limit(4)
 
 /*Find the product name and product material of each products*/
-SELECT product_name, product_material FROM Products;
+db.products.find({}, { name: 1, material: 1, _id: 0 })
 
 /*Find the product with a row id of 10*/
-SELECT * FROM Products WHERE product_id = 10;
+db.products.findOne({ row_id: 10 })
 
 /*Find only the product name and product material*/
-SELECT product_name, product_material FROM Products;
+db.products.find({}, { name: 1, material: 1, _id: 0 })
 
 /*Find all products which contain the value of soft in product material */
-SELECT * FROM Products WHERE product_material LIKE '%soft%';
+db.products.find({ material: /soft/i })
 
 /*Find products which contain product color indigo  and product price 492.00*/
-SELECT * FROM Products WHERE product_color = 'indigo' AND product_price = 492.00;
+db.products.find({ color: "indigo", price: 492.00 })
 
 /*Delete the products which product price value are same*/
-DELETE FROM Products WHERE product_price IN (
-  SELECT product_price FROM (
-    SELECT product_price, COUNT(*) as count FROM Products GROUP BY product_price HAVING count > 1
-  ) as duplicates
-);
+db.products.deleteMany({ price: { $eq: 492.00 } })
+
